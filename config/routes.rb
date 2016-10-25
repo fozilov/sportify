93c2372b7f7e6471eafe3ab6de2a9106c6f7aa7b
fozilov <fozilov@sansan.com>
2016-10-26 07:12:47 +0900

    Final first commit xD

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :feeds do
+    member do
+      resources :entries, only: [:index, :show]
+    end
+  end
+
+  resources :feeds
+  root 'feeds#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
